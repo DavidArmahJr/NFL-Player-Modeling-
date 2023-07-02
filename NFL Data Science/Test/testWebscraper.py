7d3f7377a5ad6531cf6439fd8608d3fb2953750d
DavidArmahJr <111519747+DavidArmahJr@users.noreply.github.com>
2023-07-02 19:29:20 -0400

    Update testWebscraper.py

--- NFL Data Science/Test/testWebscraper.py
+++ NFL Data Science/Test/testWebscraper.py
@@ -1,1 +1,14 @@
 
+
+
+# Test the Section 1 Method with various cases
+def testConfigure(url):
+    examDF = configure(url)
+    assert examDF is not None
+
+# Good Link Test
+testConfigure('https://www.footballdb.com/statistics/nfl/player-stats/passing/2022/regular-season?sort=passrate&limit=all')
+#rushing)
+# Bad Link Test
+testConfigure('https://www.footbaldb.com/statistics/nfl/player-stats/passing/2022/regular-season?sort=passrate&limit=all')
+   
